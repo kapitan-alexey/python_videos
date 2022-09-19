5981a1320c019268cd20d5b2be63004644bb329c
Alexey Shmateyev <88682105+kapitan-alexey@users.noreply.github.com>
2022-09-19 09:52:25 +0600

    Update youtube_client.py

--- get_videos_service/youtube_client.py
+++ get_videos_service/youtube_client.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 
 import requests
 from config import YOUTUBE_API_ENDPOINT
@@ -22,7 +23,7 @@ def get_youtube_videos(channel_id: str, api_key: str) -> dict:
 
 def parse_videos(
     videos_dict: dict, existing_videos: list, channel_id: str
-) -> list[Video]:
+) -> List[Video]:
 
     youtube_videos: list = []
 
