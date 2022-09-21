1620b5eee2e49548163b4dc7a503771a2a12d1e3
Alexey Shmateyev <88682105+kapitan-alexey@users.noreply.github.com>
2022-09-21 20:09:32 +0600

    Update config.py

--- get_videos_service/config.py
+++ get_videos_service/config.py
@@ -4,5 +4,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
-YOUTUBE_API_KEY = "AIzaSyAteUfXHa2_-2QEe12e1cYyFXY3cKmXUa4"
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
