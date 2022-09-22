b2f4e33b8b409fc3e245948754c7473517bd3f27
kapitan-alexey <Shmateyevne@gmail.com>
2022-09-22 23:22:10 +0600

    fixed bug with existing videos trying to be saved in db

--- get_videos_service/app.py
+++ get_videos_service/app.py
@@ -6,12 +6,13 @@ channels = get_channels()
 
 
 videos_in_bd = get_videos()
-ids_of_videos_in_bd = [video.id for video in videos_in_bd]
+ids_of_videos_in_bd = [video.youtube_link for video in videos_in_bd]
+videos_youtube_ids = [video_link.split('=')[1] for video_link in ids_of_videos_in_bd]
 
 new_videos: list = []
 for channel in channels:
     youtube_response = get_youtube_videos(channel, YOUTUBE_API_KEY)
-    channel_videos = parse_videos(youtube_response, ids_of_videos_in_bd, channel)
+    channel_videos = parse_videos(youtube_response, videos_youtube_ids, channel)
     new_videos.append(channel_videos)
 
 save_new_videos(new_videos)
